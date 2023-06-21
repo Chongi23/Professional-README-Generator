8c8ecc29b0a303af250268c916243ca219ff71ba
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-06-21 01:08:19 -0500

    added final code, and video to read me

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const path = require('path');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path")
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        //Github user name inatake
+    
+        {
+            type: 'input',
+            name: 'title',
+            message: "What is your project's title?",
+        },
+    
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a short description of your project:',
+         },
+    
+         {
+            type: 'list',
+            name: 'license',
+            message: 'What type of license should your project have??',
+            choices: ['MIT', 'APACHE 2.0', 'BSD 3', 'None'],
+         },
+         {
+            type: 'input',
+            name: 'installation',
+            message: 'What command should be run to install dependencies??',
+            default: 'npm init',
+         },
+         {
+            type: 'input',
+            name: 'useage',
+            message: 'What does the user need to know about using the repo??',
+         },
+         {
+            type: 'input',
+            name: 'contribution',
+            message: 'What should the user know about contribution to the repo??',
+         },
+         {
+            type: 'input',
+            name: 'test',
+            massage: 'What command is used to run tests??',
+            default: 'N/A',
+         },
+        {
         type: 'input',
         name: 'github',
         message: 'What is your GitHub username??',
@@ -17,61 +57,19 @@ const questions = [
         name: 'email',
         message: 'What is your email address??'
     },
-    {
-        type: 'input',
-        name: 'title',
-        message: "What is your project's title?",
-    },
-
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Write a short description of your project:',
-     },
-
-     {
-        type: 'list',
-        name: 'license',
-        message: 'What type of license should your project have??',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-     },
-     {
-        type: 'input',
-        name: 'installation',
-        message: 'What command should be run to install dependencies??',
-        default: 'npm init',
-     },
-     {
-        type: 'input',
-        name: 'useage',
-        message: 'What does the user need to know about using the repo??',
-     },
-     {
-        type: 'input',
-        name: 'contribution',
-        message: 'What should the user know about contribution to the repo??',
-     },
-     {
-        type: 'input',
-        name: 'test',
-        massage: 'What command is used to run tests??',
-        default: 'npm test',
-     },
+   
 ];
 
 // TODO: Create a function to write README file..based off user input
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(). fileName). data, "utf8");
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
-//The path.join() method joins all given path segments together using the platform-specific separator as a delimiter, then normalizes the resulting path.
-//process.cwd() gives the name of the directory inside which the NodeJS app is being served from. In other words, the working directory of the NodeJS process.
-
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt(questions).then((inquirerResponses) => {
+inquirer.prompt(questions).then((responses) => {
     console.log('Generating README...');
-    writeToFile('README.md', generateMarkdown({...inquirerResponses}))
+    writeToFile('README.md', generateMarkdown({...responses}))
 });
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,77 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github licnese](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
+  }
+ return "";
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+
 function renderLicenseLink(license) {}
+ 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
+function renderLicenseSection(license) {
+  return `[![License](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `${data.title}
+  ${renderLicenseSection(data.license)}
 
-`;
+  
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [License](#license)
+  * [Questions](#questions)
+  
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage 
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ##License
+  This project is licensed under the ${data.license}
+
+  ##Badges
+  
+  ${renderLicenseBadge(data.license)}
+
+  
+  ## Questions
+  
+  * [Github](https://github.com/${data.github})
+  * [Email](${data.email})
+  * For any further questions, contact me at: [email](${data.email})
+  
+  ##Contribution
+  
+
+  ##Tests
+
+  ${data.tests}
+  `;
+
 }
-
+// should it be renderLicenseSection?? test app and check 
 module.exports = generateMarkdown;
